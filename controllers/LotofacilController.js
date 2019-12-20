82b0abd8d7bf11032dc7e1809ef2a76f53e6a065
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-20 13:10:47 -0300

    Update LotofacilController.js

--- controllers/LotofacilController.js
+++ controllers/LotofacilController.js
@@ -1,7 +1,7 @@
 'use strict';
 var jsdom = require('jsdom');
 
-const url = "http://www1.caixa.gov.br/loterias/loterias/lotofacil/lotofacil_pesquisa_new.asp";
+const url = "http://www.loterias.caixa.gov.br/wps/portal/loterias/landing/quina/!ut/p/a1/jc69DoIwAATgZ_EJepS2wFgoaUswsojYxXQyTfgbjM9vNS4Oordd8l1yxJGBuNnfw9XfwjL78dmduIikhYFGA0tzSFZ3tG_6FCmP4BxBpaVhWQuA5RRWlUZlxR6w4r89vkTi1_5E3CfRXcUhD6osEAHA32Dr4gtsfFin44Bgdw9WWSwj/dl5/d5/L2dJQSEvUUt3QS80SmlFL1o2XzYxTDBIMEcwSjBJMjgwQTRFUDJWSlYzMDM1/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_61L0H0G0J0VSC0AC4GLFAD20G6/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1565647821868";
 const query = "?submeteu=sim&opcao=concurso&txtConcurso=";
 
 module.exports = {
@@ -21,53 +21,7 @@ module.exports = {
                     return res.status(404).json({ error: "Resultado não encontrado" });
                 }
 
-                var retorno = {
-                    nome:"LOTOFÁCIL",
-                    numero_concurso:html[0],
-                    data_concurso:html[34],
-                    data_concurso_milliseconds:1562122800000,
-                    local_realizacao:html[31],
-                    rateio_processamento:false,
-                    acumulou:false,
-                    valor_acumulado:0,
-                    dezenas:[html[3],
-                             html[4],
-                             html[5],
-                             html[6],
-                             html[7],
-                             html[8],
-                             html[9],
-                             html[10],
-                             html[11],html[12],html[13],html[14],html[15],html[16],html[17]],
-                    teste: html[3],
-                    numero: html[0],//ok
-                    data: html[11],
-                    cidade: html[12] + '/' + html[13],
-                    local: html[14],
-                    valorAcumulado: html[1],
-                    dezenas: dezenas.slice(6, 12),
-                    premiacao: {
-                        sena: {
-                            ganhadores: html[3],
-                            valorPago: html[4]
-                        },
-                        quina: {
-                            ganhadores: html[5],
-                            valorPago: html[6]
-                        },
-                        quadra: {
-                            ganhadores: html[7],
-                            valorPago: html[8]
-                        }
-                    },
-                    arrecadacaoTotal: html[24],
-                    proximoConcurso: {
-                        data: html[22],
-                        valorEstimado: html[21],
-                    },
-                    valorAcumuladoFinalCinco: html[18],
-                    valorAcumuladoMegaVirada: html[23]
-                };
+                var retorno = html;
 
                 res.json(retorno);
             }
