1ab217cf49b72d0c4009cb4b0e7ee3e0730fffb8
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-21 10:25:03 -0300

    Update MegasenaController.js

--- controllers/MegasenaController.js
+++ controllers/MegasenaController.js
@@ -21,6 +21,55 @@ module.exports = {
                     return res.status(404).json({ error: "Resultado não encontrado" });
                 }
 
+                var retorno = {  
+                    nome:"MEGA-SENA",
+                    numero_concurso:html[0],
+                    data_concurso:html[11],
+                    data_concurso_milliseconds:1562122800000,
+                    local_realizacao:html[12],
+                    rateio_processamento:false,
+                    acumulou:false,
+                    valor_acumulado:html[1],
+                    dezenas:[dezenas.slice(6, 12)],
+                    premiacao:[  
+                       {  
+                          nome:"Sena",
+                          quantidade_ganhadores:html[3],
+                          valor_total:html[4],
+                          acertos:6
+                       },
+                       {  
+                          nome:"Quina",
+                          quantidade_ganhadores:html[5],
+                          valor_total:html[6],
+                          acertos:5
+                       },
+                       {  
+                          nome:"Quadra",
+                          quantidade_ganhadores:html[7],
+                          valor_total:html[8],
+                          acertos:4
+                       }
+                    ],
+                    local_ganhadores:[  
+                       {  
+                          local:html[14],
+                          cidade:html[12],
+                          uf:"NEHUM",
+                          quantidade_ganhadores:1,
+                          canal_eletronico:false
+                       }
+                    ],
+                    arrecadacao_total:html[24],
+                    data_proximo_concurso: html[22],
+                    data_proximo_concurso_milliseconds:1562382000000,
+                    valor_estimado_proximo_concurso:html[21],
+                    valor_final_concurso_acumulado:html[18],
+                    numero_final_concurso_acumulado:0,
+                    valor_acumulado_especial:html[23],
+                    nome_acumulado_especial:"Mega da Virada"
+                 };
+                /*
                 var retorno = {
                     numero: html[0],
                     data: html[11],
@@ -50,6 +99,7 @@ module.exports = {
                     valorAcumuladoFinalCinco: html[18],
                     valorAcumuladoMegaVirada: html[23]
                 };
+                 */
 
                 res.json(retorno);
             }
