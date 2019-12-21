17d1bb85d2dfe72570d8e0f8564ab8afde050765
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-21 11:24:54 -0300

    Update DuplasenaController.js

--- controllers/DuplasenaController.js
+++ controllers/DuplasenaController.js
@@ -21,35 +21,99 @@ module.exports = {
                     return res.status(404).json({ error: "Resultado não encontrado" });
                 }
 
-                var retorno = {
-                    numero: html[0],
-                    data: html[11],
-                    cidade: html[12] + '/' + html[13],
-                    local: html[14],
-                    valorAcumulado: html[1],
-                    dezenas: dezenas.slice(6, 12),
-                    premiacao: {
-                        sena: {
-                            ganhadores: html[3],
-                            valorPago: html[4]
-                        },
-                        quina: {
-                            ganhadores: html[5],
-                            valorPago: html[6]
-                        },
-                        quadra: {
-                            ganhadores: html[7],
-                            valorPago: html[8]
-                        }
-                    },
-                    arrecadacaoTotal: html[24],
-                    proximoConcurso: {
-                        data: html[22],
-                        valorEstimado: html[21],
-                    },
-                    valorAcumuladoFinalCinco: html[18],
-                    valorAcumuladoMegaVirada: html[23]
-                };
+                var retorno = {  
+                    nome:"DUPLASENA",
+                    numero_concurso:1943,
+                    data_concurso:"2019-06-01T00:00:00-03:00",
+                    data_concurso_milliseconds:1559358000000,
+                    local_realizacao:"S\u00c3O PAULO, SP",
+                    rateio_processamento:false,
+                    acumulou:false,
+                    valor_acumulado:91580.88,
+                    dezenas:[  
+                       "06",
+                       "08",
+                       "22",
+                       "25",
+                       "38",
+                       "48"
+                    ],
+                    dezenas_2:[  
+                       "04",
+                       "13",
+                       "20",
+                       "31",
+                       "35",
+                       "42"
+                    ],
+                    premiacao:[  
+                       {  
+                          nome:"Sena",
+                          quantidade_ganhadores:1,
+                          valor_total:4379565.38,
+                          acertos:6
+                       },
+                       {  
+                          nome:"Quina",
+                          quantidade_ganhadores:36,
+                          valor_total:2312.65,
+                          acertos:5
+                       },
+                       {  
+                          nome:"Quadra",
+                          quantidade_ganhadores:1315,
+                          valor_total:72.35,
+                          acertos:4
+                       },
+                       {  
+                          nome:"Terno",
+                          quantidade_ganhadores:23626,
+                          valor_total:2.01,
+                          acertos:3
+                       }
+                    ],
+                    premiacao_2:[  
+                       {  
+                          nome:"Sena",
+                          quantidade_ganhadores:0,
+                          valor_total:0,
+                          acertos:6
+                       },
+                       {  
+                          nome:"Quina",
+                          quantidade_ganhadores:16,
+                          valor_total:4683.12,
+                          acertos:5
+                       },
+                       {  
+                          nome:"Quadra",
+                          quantidade_ganhadores:1131,
+                          valor_total:84.12,
+                          acertos:4
+                       },
+                       {  
+                          nome:"Terno",
+                          quantidade_ganhadores:21824,
+                          valor_total:2.17,
+                          acertos:3
+                       }
+                    ],
+                    local_ganhadores:[  
+                       {  
+                          local:"ITAJA\u00cd\/SC",
+                          cidade:"ITAJA\u00cd",
+                          uf:"SC",
+                          quantidade_ganhadores:1,
+                          canal_eletronico:false
+                       }
+                    ],
+                    arrecadacao_total:2743672,
+                    data_proximo_concurso:"2019-06-04T00:00:00-03:00",
+                    data_proximo_concurso_milliseconds:1559617200000,
+                    valor_estimado_proximo_concurso:250000,
+                    valor_acumulado_especial:1744838.08,
+                    nome_acumulado_especial:"P\u00e1scoa"
+                 };
 
                 res.json(retorno);
             }
