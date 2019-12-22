860e7ff137e12b43a6e89b9c09f8b9ec411246c4
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-22 20:40:48 -0300

    Update LotomaniaController.js

--- controllers/LotomaniaController.js
+++ controllers/LotomaniaController.js
@@ -30,7 +30,7 @@ module.exports = {
                     rateio_processamento:false,
                     acumulou:false,
                     valor_acumulado:0,
-                    dezenas:[html[6],html[7],html[8],html[9],html[10],html[11],html[12],html[13],html[14],html[15],html[16],html[17],html[18],html[19],html[20],html[21],html[22],html[23],html[24]],
+                    dezenas:[html[6],html[7],html[8],html[9],html[10],html[11],html[12],html[13],html[14],html[15],html[16],html[17],html[18],html[19],html[20],html[21],html[22],html[23],html[24],html[25]],
                     premiacao:[  
                        {  
                           nome:"20 Acertos",
