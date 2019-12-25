f0ee6b671a3b2d16f896992e87d93cef5039acdb
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-25 10:41:54 -0300

    Update FederalController.js

--- controllers/FederalController.js
+++ controllers/FederalController.js
@@ -16,6 +16,7 @@ module.exports = {
                 window.$("ul li").each(function () { dezenas.push(window.$(this).text()) });
 
                 var html = window.$("html").text().split("|");
+                var table = window.$("html").text().split("/table>");
                 
 
                 if (html.length < 4) {
@@ -24,7 +25,7 @@ module.exports = {
 
                 var retorno = {  
                     nome:"FEDERAL",
-                    numero_concurso:html[2],
+                    numero_concurso:table[0],
                     data_concurso:html[16],
                     data_concurso_milliseconds:1562382000000,
                     local_realizacao:html[3],
