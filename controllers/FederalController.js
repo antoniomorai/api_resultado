40b8cc9d3fb835e36828249606a30e3f2266b4a6
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-25 18:53:13 -0300

    Update FederalController.js

--- controllers/FederalController.js
+++ controllers/FederalController.js
@@ -1,51 +1,6 @@
-'j
-            url,
-            ["http://code.jquery.com/jquery.js"],
-            function (err, window) {
-                var dezenas = [];
-                 window.$("table td").each(function () {dezenas.push(window.$(this).text()) });
-        
-     
 
-     var html = window.$("h2").text().split("|");
-     
-    
 
-                if (html.length < 4) {
-                    return res.status(404).json({ error: "Resultado não encontrado" });
-                }
-*/
-                
-                          
-
-                
-                            ganhadores: html[3],
-                            valorPago: html[4]
-                        },
-                        quina: {
-                            ganhadores: html[5],
-                            valorPago: html[6]
-                        },
-                        quadra: {
-                            ganhadores: html[7],
-                            valorPago: html[8]
-                        }
-                    },
-                    arrecadacaoTotal: html[24],
-                    proximoConcurso: {
-                        data: html[22],
-                        valorEstimado: html[21],
-                    },
-                    valorAcumuladoFinalCinco: html[18],
-                    valorAcumuladoMegaVirada: html[23]
-                };
-
-                res.json(retorno);
-            }
-        );
-    }
-}
-frj'use strict';
+'use strict';
 var jsdom = require('jsdom');
 
 const url = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp";
