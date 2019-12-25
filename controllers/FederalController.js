5303b625226e1c0a0638eba8d8e3fe6774c5b08f
antoniomorai <57841149+antoniomorai@users.noreply.github.com>
2019-12-25 10:38:31 -0300

    Update FederalController.js

--- controllers/FederalController.js
+++ controllers/FederalController.js
@@ -1,7 +1,8 @@
 'use strict';
 var jsdom = require('jsdom');
 
-const url = "http://www1.caixa.gov.br/loterias/loterias/federal/federal_pesquisa.asp";
+//const url = "http://www1.caixa.gov.br/loterias/loterias/federal/federal_pesquisa.asp";
+const url = "http://www.loterias.caixa.gov.br/wps/portal/loterias/landing/federal/";
 const query = "?submeteu=sim&opcao=concurso&txtConcurso=";
 
 module.exports = {
